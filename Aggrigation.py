969bfd6a9b598a8f3faebff1ed6d194d8910e419
Bowen Song <sbowen@bu.edu>
2017-10-11 16:42:33 -0400

    aggrigation

---
+++ Aggrigation.py
@@ -0,0 +1,15 @@
+#Send back to user (know when task is done)(M->client)Cloud
+
+# -Check if all the result is back
+# -Discard impossible task and log it
+# -Present brief report
+# -Log with detail
+# -Zip the result
+# -send it back
+
+import os 
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+#cwd = os.getcwd() # get the path of current fodler
+
