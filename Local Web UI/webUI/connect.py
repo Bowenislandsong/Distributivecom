106f82707935d7531fbc629b9f78f24e159a5ec2
helloimlixin <31867524+helloimlixin@users.noreply.github.com>
2017-11-01 17:58:47 -0400

    Add files via upload

---
+++ Local Web UI/webUI/connect.py
@@ -0,0 +1,14 @@
+#from subprocess import call
+
+# def pornhub():
+# 	print('welcome to pornhub')
+
+# if __name__ == '__main__':
+# 	pornhub()
+#os.system('ls')
+
+#call('ls')
+import ray
+import os
+
+os.system('ray start --redis-address 155.41.110.253:6379')
