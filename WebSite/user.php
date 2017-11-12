929b12987b3f9ff0efb2df806287aced621863e0
Marlon666 <31746015+Marlon666@users.noreply.github.com>
2017-11-12 16:09:53 -0500

    stir

--- WebSite/user.php
+++ WebSite/user.php
@@ -2,77 +2,74 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Medusa.pys</title>
+		<title>User Medusa.pys</title>
 		<link rel="stylesheet" href="css/flexboxgrid.css">
 		<link rel="stylesheet" href="css/font-awesome.css">
 		<link rel="stylesheet" href="css/style.css">
 	</head>
 	<body>
 		<!--Header-->
-		<header id="main-header">
+		<header id="main-header"> 
 			<div class="container">
-				<div class="row end-sm end-md end-lg center-xs middle-xs middle-sm middle-md middle-lg">
+				<div class="row end-sm end-md end-lg center-xs middle-xs middle-sm middle- md middle-lg">
 					<div class="col-xs-12 col-sm-2 col-md-2 col-lg-2">
 						<h1><span class="primary-text">Medusa</span>.pys</h1>
 					</div>
 					<div class="col-xs-12 col-sm-10 col-md-10 col-lg-10">
 						<nav id="navbar">
 							<ul>
-								<li class="current"><a href="index.html">Home</a></li>
+								<li><a href="index.html">Home</a></li>
 								<li><a href="provider.html">Provider</a></li>
-								<li><a href="user.php">User</a></li>
+								<li class="current"><a href="user.html">User</a></li>
 								<li><a href="contact.html">Contact</a></li>
 								<li><a href="login.html">Login</a></li>
 							</ul>
 						</nav>						
 					</div>
 				</div>
-			
+			</div>			
 		</header>
-
-	<!--showcase-->
-		<section id="showcase">
+	
+		<section id="subheader">
 			<div class="container">
-				<div class="row center-xs center-sm center-lg middle-xs middle-sm middle-lg">
-					<div class="col-xs-12 col-sm-10 col-md-10 col-lg-7 showcase-content">
-						<h1>Weclome to <span class="primary-text">Medusa</span>.pys!</h1>
-						<p>The best distributing computation platform for Data Scientists!</p>
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<h1>User</h1>						
+					</div>					
+				</div>				
+			</div>			
+		</section>
+
+	
+		<!--user uploads multiple files, add error ta -->
+		<section id="multi-upload">
+			<form action="./upload-form.php" method="post" enctype="multipart/form-data">
+				<input type="file" name="fileToUpload" id="fileToUpload">
+				<input type="submit" value="Click to Upload" name="submit">
+			</form>
+		</section>	
+
+		<!--upload a single file-->
+		<section id="single-upload">
+			<div class="container">
+				<div class="row center-xs center-sm center-md center-lg middle-xs middle-sm middle-lg">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<h2><span class="primary-text">Choice 2:</span> Let us segment for you!</h2>
+						<p>Please Upload Your File Here:</p>
+						<form id="single-upload-place" enctype="multipart/form-data" method="post" action="upload-form.php">
+							<input type="file" name="fileToUpload" id="fileToUpload" onchange="fileSelected();"/>
+						</form>	
+						<button type="sumbit">Upload</button>
+						<br>
+						<br>
+						<br>												
 					</div>
 				</div>
 			</div>
 		</section>	
 
-		<!--project features-->
-		<section id="features">
-			<div class="container">
-				<div class="row center-xs center-sm center-md center-lg bottom-xs bottom-sm bottom-md bottom-lg">
-					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-						<h2>Core Features</h2>
-						<p>What's Included</p>
-						<!--Add some simple and ugly icons-->
-						<div class="row center-xs center-sm center-md center-lg">
-							<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-								<i class="fa fa-support"></i><br>
-								<h4>Multi-OS</h4>
-								<p>Support Mac OS, Windows,and Linux Operating Systenms</p>	
-							</div>
-							<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-								<i class="fa fa-usd"></i><br>
-								<h4>Earn Money</h4>
-								<p>Contribute the GPUs of the host to gain real money</p>	
-							</div>
-							<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-								<i class="fa fa-hotel"></i><br>
-								<h4>Use Spare Time</h4>
-								<p>Utilize the spare time to do meaningful things</p>	
-							</div>							
-						</div>						
-					</div>
-				</div>				
-			</div>			
-		</section>
 
-		<!--need more infomation-->
+		
 
 		<!--Project-->
 		<section id="project">
