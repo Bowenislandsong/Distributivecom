61adccf499ec30246a6e8e53e6fe23a9fd37bc42
helloimlixin <31867524+helloimlixin@users.noreply.github.com>
2017-11-18 20:13:39 -0500

    Add files via upload

--- webUI_Networking/client.py
+++ webUI_Networking/client.py
@@ -1,91 +1,110 @@
-# client.py  
+# client.py
 
 import os
 import subprocess
 from flask import Flask, render_template, request
 import socket
 import sys
-import math
 from cryptography.fernet import Fernet
-BUFFER_SIZE = 65536
+
+BUFFER_SIZE = 1024
+# create a socket object
+# get local machine name / ip
+host = socket.gethostname()
+port = 9999
+
 
 def encryption(message):
-	message_b=bytes(message,encoding="utf8")#str to bytes
-	key = Fernet.generate_key()
-	cipher_suite = Fernet(key)
-	cipher_text = cipher_suite.encrypt(message_b)
-	return cipher_text,key
+    message_b = bytes(message, encoding="utf8")  # str to bytes
+    key = Fernet.generate_key()
+    cipher_suite = Fernet(key)
+    cipher_text = cipher_suite.encrypt(message_b)
+    return cipher_text, key
 
-def send_message(encryptedMessage):
-	# create a socket object
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
-	# get local machine name / ip
-	host = socket.gethostname()   
-	# host = '155.41.92.21'                        
+def send_message(message, s):
+    sendStr = str(message)
+    s.send(sendStr.encode('ascii'))
 
-	port = 9999
 
-	# connection to hostname on the port.
-	s.connect((host, port))                               
-	sendStr=str(encryptedMessage)
-	s.send(sendStr.encode('ascii'))
-	# Receive no more than 1024 bytes
-	tm = s.recv(BUFFER_SIZE)                                     
+def receive_message(s):
+    data = s.recv(BUFFER_SIZE)
+    return data
 
-	s.close()
-
-	print("REPLY FROM SERVER: %s" % tm.decode('ascii'))
 
 # flask initialization
 project_root = os.path.dirname(__file__)
-app = Flask(__name__, template_folder='/Users/xinli/Desktop/webUI/templates',
-	static_folder = '/Users/xinli/Desktop/webUI/static')
+app = Flask(
+    __name__,
+    template_folder='/Users/xinli/Desktop/webUI/templates',
+    static_folder='/Users/xinli/Desktop/webUI/static')
+
+
+def send_file(s):
+    file = open('file.zip', 'rb')
+    file_seg = file.read(BUFFER_SIZE)
+    while(file_seg):
+        s.send(file_seg)
+        file_seg = file.read(BUFFER_SIZE)
+    file.close()
+
+
+def receive_file(s):
+    file_seg = s.recv(BUFFER_SIZE)
+    if file_seg:
+        file = open('file1.zip', 'wb')
+        while(file_seg):
+            file.write(file_seg)
+            file_seg = s.recv(BUFFER_SIZE)
+        file.close()
+    else:
+        print('No file received.')
+
+
+def aquire_user_info():
+    name = str(request.form.get('txtuser'))
+    password = str(request.form.get('txtpass'))
+    if not name or not password:
+        return render_template('failure.html')
+    total_info = name + ' ' + password
+    return total_info
+
+
+def init_connection(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    return s
+
 
 @app.route('/')
 def login(name=None):
-	return render_template('login.html')
+    return render_template('login.html')
+
 
 @app.route('/login', methods=['POST'])
 def authentication():
-	name = str(request.form.get('txtuser'))
-	password = str(request.form.get('txtpass'))
-	combination = name + ' ' + password
-	print(combination)
-	encrypted_message, key_value = encryption(combination)
-	print(encrypted_message)
-	print(key_value)
-	# create a socket object
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-	# get local machine name / ip
-	host = socket.gethostname()   
-	# host = '155.41.92.21'                        
-	port = 9999
-	# connection to hostname on the port.
-	s.connect((host, port))                                                            
-	f = open('test.zip','rb')
-	i = 0
-	l = f.read(BUFFER_SIZE)
-	while (l):
-		i += 1
-		print(i)
-		s.send(l)
-		# Receive no more than 1024 bytes    
-		l = f.read(BUFFER_SIZE)
-	f.close()
-	s.shutdown(socket.SHUT_WR)
-	s.close()
-	# send_file(l)
-	if not name or not password:
-		return render_template('failure.html')
-	return render_template('button.html')
+    info = aquire_user_info()
+    s = init_connection(host, port)
+    send_message(info, s)
+    data = str(receive_message(s))
+    s.close()
+    real_data = data[2:len(data) - 1]
+    if real_data == 'yes':
+        return render_template('button.html')
+    else:
+        return render_template('failure.html')
+
 
 @app.route('/execute')
 def hello(name=None):
-	subprocess.call('python connect.py',shell=True)
-	return render_template('execute.html')
+    #	subprocess.call('python connect.py',shell=True)
+    s = init_connection(host, port)
+    send_message('file', s)
+    receive_file(s)
+    s.close()
+    return render_template('execute.html')
+
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
-	app.debug
-
+    app.run(host='0.0.0.0')
+    app.debug
--- webUI_Networking/server.py
+++ webUI_Networking/server.py
@@ -1,67 +1,100 @@
-import socket                                         
-import time
-import json
-import time
+import os
+import socket
 import threading
-import math
-#Global vars
+from cryptography.fernet import Fernet
+# Global vars
 _PORT = 9999
-_LISTEN_QUEUE_SIZE=100
-nlist={} #node list
-BUFFER_SIZE = 65536
+_LISTEN_QUEUE_SIZE = 100
+BUFFER_SIZE = 2000
 
-def init_server():
-	pass
 
-# def reply(addr,reply_str, clientsocket):
-# 	rdata={}
-# 	rdata['ip']=addr
-# 	rdata['time']=time.ctime(time.time())
-# 	jsonData=json.dumps(rdata)
-# 	clientsocket.send(reply_str.encode('ascii'))
+# create a socket object
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# get local machine name / ip
+host = socket.gethostname()
+
+
+def decryption(encrypted_msg, key):
+    cipher_suite = Fernet(key)
+    decry_msg = cipher_suite.decrypt(encrypted_msg)
+    decry_msg = str(decry_msg, encoding="utf-8")
+    return decry_msg
+
+
+def send_file(clientsocket):
+    if os.path.isfile('test.zip'):
+        file = open('test.zip', 'rb')
+        file_seg = file.read(BUFFER_SIZE)
+        while(file_seg):
+            clientsocket.send(file_seg)
+            file_seg = file.read(BUFFER_SIZE)
+        file.close()
+    else:
+        print('No such file')
+
+
+
+def receive_file(clientsocket):
+    file = open('file1.zip', 'wb')
+    file_seg = clientsocket.recv(BUFFER_SIZE)
+    while(file_seg):
+        file.write(file_seg)
+        file_seg = clientsocket.recv(BUFFER_SIZE)
+    file.close()
+
+
+def send_message(message, clientsocket):
+    sendStr = str(message)
+    clientsocket.send(sendStr.encode('ascii'))
+
+
+def receive_message(clientsocket):
+    data = clientsocket.recv(BUFFER_SIZE)
+    return data
+
+
+def split_data(data):
+    real_data = data[1:len(data)]
+    d_split = real_data.split()
+    content = d_split[0]
+    key = d_split[1]
+    content = bytes(content, encoding="utf8")
+    key = bytes(key, encoding="utf8")
+    return content, key
+
+
+def execution(clientsocket):
+    data = str(receive_message(clientsocket))
+    real_data = data[2:len(data) - 1]
+    if real_data == 'file':
+        send_file(clientsocket)
+
+    else:
+        mdata = real_data.split()
+        name = mdata[0]
+        password = mdata[1]
+        if name == '123' and password == '456':
+            send_message('yes', clientsocket)
+
+    clientsocket.close()
 
 
 def listen_thread():
-	# create a socket object
-	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    # bind to the port
+    serversocket.bind((host, _PORT))
+    # queue up to 100 requests
+    while True:
+        serversocket.listen(_LISTEN_QUEUE_SIZE)
+        while True:
+            # establish a connection
+            clientsocket, addr = serversocket.accept()
+            threading.Thread(target=execution, args=(clientsocket,)).start()
 
-	# get local machine name / ip
-	host = socket.gethostname()                           
-	# bind to the port
-	serversocket.bind((host, _PORT))                                  
-
-	# queue up to 100 requests
-	serversocket.listen(_LISTEN_QUEUE_SIZE)
-                                    
-
-	while True:
-	    # establish a connection
-	    clientsocket,addr = serversocket.accept()   
-
-	    print("Got a connection from %s" % str(addr[0]))
-	    reply_str = "Success" + "\r\n"
-	    f = open('result.zip','wb')
-	    l = clientsocket.recv(BUFFER_SIZE)
-	    i = 0
-	    while (l):
-	    	f.write(l)
-	    	i += 1
-	    	print(i)
-	    	l = clientsocket.recv(BUFFER_SIZE)
-	    f.close()
-	    # reply(addr,reply_str)
-	    # f.write(l)
-	    # reply(addr,reply_str, clientsocket)
-
-	    clientsocket.close()
-	
-	# serversocket.close()
 
 def main():
-    t1 = threading.Thread(target=listen_thread)
-    t1.start()
-    t1.join()
-    print("EXITING")
+    listen_thread()
+
 
 if __name__ == "__main__":
     main()
