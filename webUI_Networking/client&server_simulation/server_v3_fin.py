7641b0fb2aaa61228a4dc17fc88ddbb79db6c0e8
yzy123 <31748059+yzy123@users.noreply.github.com>
2017-11-20 17:11:19 -0500

    Update server_v3_fin.py

--- webUI_Networking/client&server_simulation/server_v3_fin.py
+++ webUI_Networking/client&server_simulation/server_v3_fin.py
@@ -8,13 +8,15 @@ _PORT = 9999
 _LISTEN_QUEUE_SIZE = 100
 BUFFER_SIZE = 2000
 q = queue.Queue()
+p = queue.Queue()
 q.put(0)
+p.put(0)
 # create a socket object
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 # get local machine name / ip
 host = socket.gethostname()
-FILEPATH = os.getcwd()+'/Data_Spliting/'
+FILEPATH = '/Users/yangzhiyi/Desktop/webUI_1/simulation'
 
 
 def decryption(encrypted_msg, key):
@@ -33,7 +35,7 @@ def send_file(clientsocket,q,filelist):
     # print(counter)
     if len(filelist) > 0 and len(filelist) > counter:
         filename = filelist[counter]
-        file = open(FILEPATH+filename, 'rb')
+        file = open(filename, 'rb')
         file_seg = file.read(BUFFER_SIZE)
         while(file_seg):
             clientsocket.send(file_seg)
@@ -48,7 +50,7 @@ def send_file(clientsocket,q,filelist):
 
 
 def receive_file(clientsocket):
-    file = open('received.zip', 'wb')
+    file = open('file1.zip', 'wb')
     file_seg = clientsocket.recv(BUFFER_SIZE)
     while(file_seg):
         file.write(file_seg)
@@ -76,7 +78,7 @@ def split_data(data):
     return content, key
 
 
-def execution(clientsocket,q,filelist):
+def execution(clientsocket,q,filelist,p):
     # initialize a counter for file transfer
     old_data = receive_message(clientsocket)
     data=str(old_data)
@@ -87,9 +89,13 @@ def execution(clientsocket,q,filelist):
         send_file(clientsocket,q,filelist)
     elif (str(data[2:4]) == 'PK'):
         print('111111111111')
-        file = open('result_server.zip', 'wb')
+        cout = p.get()
+        fname = 'result_server' + str(cout) + '.zip'
+        file = open(fname, 'wb')
         file.write(old_data)
         file.close()
+        cout = cout + 1
+        p.put(cout)
     else:
         mdata = real_data.split()
         name = mdata[0]
@@ -104,7 +110,7 @@ def listen_thread():
     filelist = []
     listOfFiles = os.listdir(FILEPATH)
     listOfFiles.sort()
-    pattern = 'Distribute*'
+    pattern = 'file*'
     for entry in listOfFiles:
         if fnmatch.fnmatch(entry, pattern):
             filelist.append(entry)
@@ -117,7 +123,7 @@ def listen_thread():
         while True:
             # establish a connection
             clientsocket, addr = serversocket.accept()
-            threading.Thread(target=execution, args=(clientsocket,q,filelist)).start()
+            threading.Thread(target=execution, args=(clientsocket,q,filelist,p)).start()
 
 
 def main():
